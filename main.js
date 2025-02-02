904bfdaf55485ea041ce8cb420d91f9fd11c303f
syswraith <syswraith@proton.me>
2025-02-02 13:44:05 +0530

    redid the localauth thing

--- main.js
+++ main.js
@@ -9,6 +9,10 @@ const client = new Client({
 
 client.on('ready', () => console.log('Client is ready!'));
 
+client.on('message_create', async msg => {
+    console.log(msg.body)
+});
+
 client.on('qr', qr => qrcode.generate(qr, { small: true }));
 
 client.initialize();
