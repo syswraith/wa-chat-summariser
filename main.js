90dd88c8951523a660f87ac6d39721bd7635f910
syswraith <syswraith@proton.me>
2025-02-02 14:29:44 +0530

    fetch messages

--- main.js
+++ main.js
@@ -10,7 +10,12 @@ const client = new Client({
 client.on('ready', () => console.log('Client is ready!'));
 
 client.on('message_create', async msg => {
-    console.log(msg.body)
+    if (msg.body.startsWith("!summarise") && msg.fromMe)
+    {
+	let number_of_messages = msg.body.split(" ")[1]
+	let chat = await msg.getChat()
+	let asc_messages = await chat.fetchMessages({limit: number_of_messages})
+    }
 });
 
 client.on('qr', qr => qrcode.generate(qr, { small: true }));
